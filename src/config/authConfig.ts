7f91ea3ed4d0f00942e80fc4b02068ec9bc56904
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-10 21:22:47 -0400

    Adds firebase auth config

---
+++ src/config/authConfig.ts
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+
+const authConfig = firebase.initializeApp({
+  apiKey: "AIzaSyB7DzStBWvQJs2aWb0l5dUlGycQ1lTEWOw",
+  authDomain: "ce-project-693cb.firebaseapp.com",
+  databaseURL: "https://ce-project-693cb.firebaseio.com",
+  projectId: "ce-project-693cb",
+  storageBucket: "ce-project-693cb.appspot.com",
+  messagingSenderId: "734320226447",
+  appId: "1:734320226447:web:5e01add310e753b12780fa",
+  measurementId: "G-F5CTFPSTKW",
+});
+
+export default authConfig;
