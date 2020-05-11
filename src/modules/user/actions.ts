64830650f38b706fa9849f2a90665dbb53f81471
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-11 00:10:34 -0400

    Adds login with google

--- src/modules/user/actions.ts
+++ src/modules/user/actions.ts
@@ -1,10 +1,19 @@
 import types from "./types";
 import { Dispatch } from "redux";
+import appAuth from "../../config/appAuth";
+import firebase from "firebase";
 
 // Update later
 export const login = () => {
   return (dispatch: Dispatch) => {
     dispatch({ type: types.LOGIN_LOADING });
-    dispatch({ type: types.LOGIN_SUCCESS, payload: {} });
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    // TODO: update this to async await
+    appAuth
+      .auth()
+      .signInWithRedirect(provider)
+      .then((user) => dispatch({ type: types.LOGIN_SUCCESS, payload: user }))
+      .catch((err) => dispatch({ type: types.LOGIN_ERROR, error: err }));
   };
 };
