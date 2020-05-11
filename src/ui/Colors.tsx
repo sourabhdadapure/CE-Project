ad56be07b0ec6b54347ff02c27e373c8ce22573b
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-11 17:07:47 -0400

    Adds colors

---
+++ src/ui/Colors.tsx
@@ -0,0 +1,33 @@
+export const Colors = {
+  Background: {
+    Primary: "#092181",
+    Secondary: "#2B77AD",
+  },
+  Buttons: {
+    Primary: "#78CCFB",
+    PrimaryShadow: "#B1DEFC",
+    White: "#FFFFFF",
+    Blue: "#5289fe",
+    CTA: "#474951",
+    DarkGray: "#19191C",
+    Gray: "#828994",
+    Active: "#5289fe",
+  },
+  Icons: {
+    White: "#ffffff",
+    Gray: "#7a7c86",
+    Black: "#000000",
+    Blue: "#5289fe",
+  },
+  Status: {
+    Fair: "#FFC828",
+    Good: "#00FFDC",
+  },
+  Labels: {
+    White: "#ffffff",
+    Gray: "#98A0B4",
+    DarkGray: "#7A7C86",
+    Link: "#5289fe",
+    Black: "#000000",
+  },
+};
