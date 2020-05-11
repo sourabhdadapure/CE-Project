e6f3484fd780a7e3a4879294b4369c5d0b133996
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-10 21:14:02 -0400

    Adds Buttons component

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+  })
+);
+
+interface ButtonProperties {
+  title: string;
+  onPress(): void;
+  style?: any;
+}
+
+function OutlinedButton(props: ButtonProperties) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Button
+        style={props.style}
+        onClick={props.onPress}
+        variant="outlined"
+        color="secondary">
+        {props.title}
+      </Button>
+    </div>
+  );
+}
+
+function ContainedButton(props: ButtonProperties) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Button
+        style={props.style}
+        onClick={props.onPress}
+        variant="contained"
+        color="secondary">
+        {props.title}
+      </Button>
+    </div>
+  );
+}
+
+export const Buttons = {
+  Outlined: (props: ButtonProperties) => (
+    <OutlinedButton
+      {...props}
+      style={props.style}
+      title={props.title}
+      onPress={props.onPress}
+    />
+  ),
+
+  Contained: (props: ButtonProperties) => (
+    <ContainedButton
+      {...props}
+      style={props.style}
+      title={props.title}
+      onPress={props.onPress}
+    />
+  ),
+};
