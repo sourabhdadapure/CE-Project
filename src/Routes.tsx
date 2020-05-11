40113df4a65453932aa1059afe690e06efa0f537
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-10 20:35:59 -0400

    initialized routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import ConfigureStore from "../src/config/configStore";
 import { Provider } from "react-redux";
+import "./App.css";
 const store = ConfigureStore();
 // import "./style.css";
 
@@ -10,7 +11,20 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <Router>
-          <div></div>
+          <div className="App">
+            <header className="App-header">
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer">
+                Learn React
+              </a>
+            </header>
+          </div>
         </Router>
       </Provider>
     );
