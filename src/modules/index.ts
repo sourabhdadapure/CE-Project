0138cc6bbdc26b9035ec7db92abb6935bd0bfebf
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-11 00:56:27 -0400

    renamed module

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user";
+import budget from "./budget";
 
 const rootReducer = combineReducers({
   user,
+  budget,
 });
 
 export default rootReducer;
