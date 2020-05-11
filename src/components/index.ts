2f0b9c475e0d5a75628bb87e2e7dc16d4bfac887
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-10 21:29:03 -0400

    Adds splas view

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Buttons } from "./Buttons";
+export { HeaderBar } from "./HeaderBar";
+export { InputField } from "./InputField";
+export { PaperComponent as Paper } from "./Paper";
+export { SliderComp as Slider } from "./Slider";
---
+++ src/views/Splash.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Typography>Splash</Typography>
+      </div>
+    );
+  }
+}
