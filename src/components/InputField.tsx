8c88771a63c11e1d37866daea2e1b6e205bb1fe5
sourabhdadapure <sourabhdadapure@gmail.com>
2020-05-12 16:03:38 -0400

    updates InputField

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,14 +2,36 @@ import React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+import UI from "../ui";
 
+const Palette = UI.Colors;
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       "& > *": {
         margin: theme.spacing(1),
         width: 300 + theme.spacing(5) * 2,
+        "& label.Mui-focused": {
+          color: Palette.Background.Secondary,
+        },
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": {
+            borderColor: Palette.Labels.White,
+          },
+          "&:hover fieldset": {
+            borderColor: Palette.Labels.Gray,
+          },
+          "&.Mui-focused fieldset": {
+            borderColor: Palette.Background.Secondary,
+          },
+        },
       },
+    },
+    inputStyle: {
+      color: Palette.Labels.White,
+    },
+    focused: {
+      color: Palette.Background.Primary,
     },
   })
 );
@@ -32,6 +54,10 @@ export const InputField: React.FC<InputFieldProperties> = ({
   return (
     <div className={classes.root}>
       <TextField
+        inputProps={{ className: classes.inputStyle }}
+        InputLabelProps={{
+          className: classes.inputStyle,
+        }}
         type={inputType}
         onChange={(event) => onChange(event)}
         id="outlined-basic"
